d6c965d28fa5f27aca2e11e3f40afc04fdcd8183
Amélie <amelie.antigny@free.fr>
2020-05-28 21:27:59 +0100

    Création de la classe Login avec connection avec la bd firebase

---
+++ app/src/main/java/com/example/homecare/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.homecare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_login);
+
+    }
+}

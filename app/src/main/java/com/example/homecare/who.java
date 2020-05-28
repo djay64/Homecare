bb4ed6bad7d09115dfd7b27935f4063ef822f2db
Jerome <jeromedoyenard@hotmail.com>
2020-05-28 02:13:05 +0200

    activity who

---
+++ app/src/main/java/com/example/homecare/who.java
@@ -0,0 +1,14 @@
+package com.example.homecare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class who extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_who);
+    }
+}

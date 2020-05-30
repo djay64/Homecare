e618264f78fc6248dc466cc80caba5e99231594c
Amélie <amelie.antigny@free.fr>
2020-05-30 16:00:12 +0100

    commencement des vue, accueille prorpiétaire

--- app/src/main/java/com/example/homecare/MainProprietaireActivity.java
+++ app/src/main/java/com/example/homecare/MainProprietaireActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -32,5 +33,9 @@ public class MainProprietaireActivity extends AppCompatActivity {
             }
         });
 
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
